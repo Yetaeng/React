a14641a146c4e9d1b434a646495cbcc21dec6c01
Yetaeng <yetaeng20@gmail.com>
2020-12-08 10:08:40 +0900

    컴포넌트 성능 최적화

--- 10/todo-app/src/App.js
+++ 10/todo-app/src/App.js
@@ -3,29 +3,25 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
   // todos 배열은 TodoListdp props로 전달됨
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos); // 파라미터를 함수 형태로 넣어줘서 컴포넌트가 처음 렌더링될 때만 함수 실행됨
 
   // 고윳값으로 사용될 id(새로운 객체를 만들 때마다 새로운 id 부여됨)
   // ref를 사용하여 변수 담기(id 값은 렌더링되는 정보가 아니고, 단순 참조용이므로)
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   // 컴포넌트 성능을 아낄 수 있도록 useCallback으로 감싼다.
   // props로 전달해야 할 함수를 만들 때는 useCallback을 사용해서 함수를 감싸도록 하자!
@@ -36,7 +32,7 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
     },
     [todos],
@@ -45,14 +41,14 @@ const App = () => {
   // id를 파라미터로 받아 와서 같은 id를 가진 항목을 todos 배열에서 지우는 함수 
   const onRemove = useCallback(
     id => {
-      setTodos(todos.filter(todo => todo.id !== id));
+      setTodos(todos => todos.filter(todo => todo.id !== id));
     },
     [todos],
   );
 
   const onToggle = useCallback(
     id => {
-      setTodos(
+      setTodos( todos => 
         todos.map(todo => 
           // map함수를 이용해 수정이 필요한 곳은 true일때 반환되는 곳에, 필요가 없는 부분은 false일때 반환되는 곳에 씀
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
--- 10/todo-app/src/components/TodoList.js
+++ 10/todo-app/src/components/TodoList.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+    const rowRenderer = useCallback(
+        ({ index, key, style }) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem
+                todo={todo}
+                key={key}
+                onRemove={onRemove}
+                onToggle={onToggle}
+                style={style}
+                />
+            );
+        },
+        [onRemove, onToggle, todos],
+    );
     return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                // map을 사용하여 컴포넌트로 변환할 때는 key props를 전달해주어야함!
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
-                // TodoListItem에서 onRemove 함수를 사용하려면 우선 TodoList 컴포넌트를 거쳐야한다.
-            ))}
-        </div>
+        <List
+            className="TodoList"
+            width={512} // 전체 크기
+            height={513} // 전체 높이
+            rowCount={todos.length} // 항목 개수
+            rowHeight={56} //  항목 높이
+            rowRenderer={rowRenderer} // 항목을 렌더링할 때 쓰는 함수(이 함수를 List 컴포넌트의 props로 설정해 주어야함)
+            list={todos} // 배열
+            style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
+        />
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList); // 최적화 작업
+// App 컴포넌트는 todos 배열이 업데이트될 때 리렌더링되는데, App 컴포넌트에 다른 state가 추가되어 해당 값들이 업데이트될 때는 
+// Todolist 컴포넌트가 불필요한 리렌더링을 할 수도 있다. 이를 방지하기위해 미리 최적화작업을하는 것이다.
