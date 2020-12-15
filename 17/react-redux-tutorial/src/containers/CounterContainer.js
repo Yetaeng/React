ff77b75264a83dafd1826c61c6dd8a1d18fac567
Yetaeng <yetaeng20@gmail.com>
2020-12-15 15:22:22 +0900

    두 번째 파라미터를 객체 형태로 넣어주기

--- 17/react-redux-tutorial/src/containers/CounterContainer.js
+++ 17/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -10,17 +10,14 @@ const CounterContainer = ({ number, increase, decrease }) => {
     );
 };
 
-// bindActionCreators 유틸 함수 사용 <- 각 액션 생성 함수를 호출하고 dispatch로 감싸는 작업을 보다 간편하게 해줌
+// mapDispatchToProps에 해당하는 파라미터를 객체 형태로 넣어주기 (가장 간편한 방법)
+// 이 방법은 connect 함수가 내부적으로 bindActionCreators작업을 대신해준다.
 export default connect(
     state => ({
         number: state.counter.number,
     }),
-    dispatch =>
-        bindActionCreators(
-            {
-                increase,
-                decrease,
-            },
-            dispatch,
-        ),
+    {
+        increase,
+        decrease,
+    },
 )(CounterContainer);
