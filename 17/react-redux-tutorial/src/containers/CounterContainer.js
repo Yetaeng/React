c63f64747310656d025a10cc4317d938b7c33b54
Yetaeng <yetaeng20@gmail.com>
2020-12-15 15:15:59 +0900

    connect 함수를 익명 함수 형태로 바꿔 선언하기

--- 17/react-redux-tutorial/src/containers/CounterContainer.js
+++ 17/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -9,24 +10,13 @@ const CounterContainer = ({ number, increase, decrease }) => {
     );
 };
 
-// mapStateToProps, mapDispatchToProps 에서 반환하는 객체 내부의 값들은 컴포넌트의 props로 전달됨
-// 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-// 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
-const mapDispatchToProps = dispatch => ({
-    // 임시 함수
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease: () => {
-        dispatch(decrease())
-    }
-});
-
-// connect 함수를 호출하여 리덕스와 연동된 컴포넌트를 만듬
+// connect 함수를 익명 함수 형태로 선언하는 방식
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch => ({
+        increase: () => dispatch(increase()),
+        decrease: () => dispatch(decrease()),
+    }),
 )(CounterContainer);
